92c25f70e2e904a2a9740c6094fd8b8683543d0d
kelly8680 <108934646+kelly8680@users.noreply.github.com>
2024-08-23 16:24:26 +0100

    Update EDA assignment by Ahmed.py

--- EDA assignment by Ahmed.py
+++ EDA assignment by Ahmed.py
@@ -1,25 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ![conf.jpeg](attachment:conf.jpeg)                   Expolratory Data Analysis on World Conflicts
-
-#                                    Introduction
-# World conflicts represent violent activities occurring across different regions globally, often involving wars, civil unrest and significant societal disorder. This leads to displacement of people, destruction of infrastructure and economic hardship.
-
-#                                Analysis Objective
-# This project is to clean dataset, which involves handling of outliers and missing values and analyze the dataset to derive insights.
-
-#                                Column description
-# Entity: Represents the region (e.g., Africa, Americas, Asia and Oceania, Europe, Middle East).
-# Year:   The year when the conflict data was recorded.
-# One-sided violence:  Number of incidents related to one-sided violence.
-# Non-state:  Number of incidents related to non-state violence.
-# Intrastate:  Number of incidents related to intrastate violence.
-# Extrasystemic:   Number of incidents related to extrasystemic violence.
-# Interstate:     Number of incidents related to interstate violence.
-
-# In[3]:
-
 
 #impoert the necessary libraries
 import pandas as pd
